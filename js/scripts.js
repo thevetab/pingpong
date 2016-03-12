fea0ba16a7c03a4f4699f12c45157a279d666d6c
Matt <thevetab@gmail.com>
2016-03-12 12:01:59 -0800

    debug scripts file and clean up indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 var pingpong_form = function(yourNumber) {
   for ( var i = 1; i <= yourNumber; i += 1) {
-   if ((i % 3 === 0) && (i % 5 === 0))  {
+   if ((i % 3 === 0) && (i % 5 === 0)) {
       $("#result").append("<p>pingpong</p>");
-    } else if (i % 5 === 0)  {
+    } else if (i % 5 === 0) {
       $("#result").append("<p>pong</p>");
-    } else if (i % 3 === 0)  {
+    } else if (i % 3 === 0) {
       $("#result").append("<p>ping</p>");
     } else {
       $("#result").append("<p>" + i + "</p>");
@@ -14,16 +14,10 @@ var pingpong_form = function(yourNumber) {
 
   $(document).ready(function() {
     $("form#pingpong_form").submit(function(event) {
-        var yourNumber = parseInt($("input#userinput").val());
-        var result = pingpong_form(yourNumber);
-
-        $(".userinput").text(yourNumber);
-
-        event.preventDefault();
-
-
-
+      var yourNumber = parseInt($("input#userinput").val());
+      var result = pingpong_form(yourNumber);
+      
+    event.preventDefault();
   });
-
 });
 
